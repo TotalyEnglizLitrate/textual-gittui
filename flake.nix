6237c209e46a732be6c62e1fe8b1c985cbfc5c73
Narendra S <narendra.s1232@gmail.com>
2025-07-22 02:04:26 +0530

    Setup base structure

---
+++ flake.nix
@@ -0,0 +1,118 @@
+{
+  description = "Development shells and package for textual-gittui";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    uv2nix,
+    pyproject-nix,
+    pyproject-build-systems,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      inherit (nixpkgs) lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+      python = pkgs.python312;
+
+      workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+      pyprojectOverrides = final: prev: {
+        # Build fixups here if needed
+      };
+
+      pythonSet =
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope
+        (
+          lib.composeManyExtensions [
+            pyproject-build-systems.overlays.default
+            overlay
+            pyprojectOverrides
+          ]
+        );
+    in {
+      packages = let
+        inherit (pkgs.callPackage pyproject-nix.build.util {}) mkApplication;
+      in {
+        default = mkApplication {
+          venv = pythonSet.mkVirtualEnv "application-env" workspace.deps.default;
+          package = pythonSet.textual-gittui;
+        };
+      };
+
+      apps = {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/textual-gittui";
+        };
+      };
+
+      devShells = {
+        default = let
+          editableOverlay = workspace.mkEditablePyprojectOverlay {
+            root = "$REPO_ROOT";
+          };
+          editablePythonSet = pythonSet.overrideScope (
+            lib.composeManyExtensions [
+              editableOverlay
+              (final: prev: {
+                textual-gittui = prev.textual-gittui.overrideAttrs (old: {
+                  src = lib.fileset.toSource {
+                    root = old.src;
+                    fileset = lib.fileset.unions [
+                      (old.src + "/pyproject.toml")
+                      (old.src + "/README.md")
+                      (old.src + "/src/textual_gittui/__init__.py")
+                    ];
+                  };
+                  nativeBuildInputs =
+                    old.nativeBuildInputs
+                    ++ final.resolveBuildSystem {
+                      editables = [];
+                    };
+                });
+              })
+            ]
+          );
+          virtualenv = editablePythonSet.mkVirtualEnv "textual-gittui-dev-env" workspace.deps.all;
+        in
+          pkgs.mkShell {
+            packages = [
+              virtualenv
+              pkgs.uv
+            ];
+            env = {
+              UV_NO_SYNC = "1";
+              UV_PYTHON = python.interpreter;
+              UV_PYTHON_DOWNLOADS = "never";
+            };
+            shellHook = ''
+              unset PYTHONPATH
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
+            '';
+          };
+      };
+    });
+}
