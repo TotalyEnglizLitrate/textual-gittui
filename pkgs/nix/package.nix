903fa8e9f995a34ce28bc9be135e609681365661
Narendra S <narendra.s1232@gmail.com>
2025-07-25 23:41:19 +0530

    screwed up nix package metadata; fix

--- pkgs/nix/package.nix
+++ pkgs/nix/package.nix
@@ -8,13 +8,16 @@
 }: let
   inherit (pkgs.callPackage pyproject-nix.build.util {}) mkApplication;
 in {
-  default = mkApplication {
-    venv = pythonSet.mkVirtualEnv "application-env" workspace.deps.default;
-    package = pythonSet.textual-gittui;
-    meta = {
-      description = "A git tui built with textual";
-      homepage = "https://github.com/TotalyEnglizLitrate/textual-gittui";
-      maintainers = with lib.maintainers; [TotalyEnglizLitrate];
+  default =
+    mkApplication {
+      venv = pythonSet.mkVirtualEnv "application-env" workspace.deps.default;
+      package = pythonSet.textual-gittui;
+    }
+    // {
+      meta = {
+        description = "A git tui built with textual";
+        homepage = "https://github.com/TotalyEnglizLitrate/textual-gittui";
+        maintainers = with lib.maintainers; [TotalyEnglizLitrate];
+      };
     };
-  };
 }
