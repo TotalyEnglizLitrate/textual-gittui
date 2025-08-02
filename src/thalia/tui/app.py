7bb514a315ad24beebfcd4fb6eb10ad33b36b8d3
Narendra S <narendra.s1232@gmail.com>
2025-08-02 12:04:25 +0530

    Add cache database support and schema initialization in Thalia app

--- src/thalia/tui/app.py
+++ src/thalia/tui/app.py
@@ -17,6 +17,7 @@ You should have received a copy of the GNU General Public License
 along with Thalia.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+import sqlite3
 from pathlib import Path
 
 from platformdirs import user_cache_path
@@ -39,12 +40,28 @@ class Thalia(App):
 
     BINDINGS = binding_loader.include_bindings("bindings")
 
-    def __init__(self, settings: conf.Settings, cache_dir: Path = user_cache_path("thalia"), **kwargs) -> None:
+    def __init__(
+        self, settings: conf.Settings, cache_db: sqlite3.Connection, cache_dir: Path | None = None, **kwargs
+    ) -> None:
         """Initialize the Thalia app."""
         self.settings = settings
-        self.cache_dir = cache_dir
+        self.cache_dir = cache_dir or user_cache_path("thalia")
+        self.cache_db = cache_db
+
+        try:
+            with cache_db:
+                schema = """
+                CREATE TABLE IF NOT EXISTS Repositories(
+                    Path TEXT PRIMARY KEY NOT NULL UNIQUE,
+                    last_accessed INTEGER NOT NULL DEFAULT (strftime('%s','now'))
+                )
+                """
+                cache_db.execute(schema)
+        except sqlite3.OperationalError:
+            exit(1)
+
         super().__init__(**kwargs)
 
     def on_mount(self) -> None:
         """Called when the app is mounted."""
-        self.push_screen(dashboard.DashboardScreen())
+        self.push_screen(dashboard.DashboardScreen(id="dashboard"))
